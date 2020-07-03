f9b9107f6b6e697c581f7d4540091546d504d616
erendiragj1 <42814751+erendiragj1@users.noreply.github.com>
2020-07-03 07:37:54 -0500

    se crea bitacoras

--- MonitorServers/appMonitoreo/views.py
+++ MonitorServers/appMonitoreo/views.py
@@ -9,16 +9,23 @@ from rest_framework.authentication import TokenAuthentication
 from .monitor import *
 import json
 
+#PATH_LOG: Se define en el archivo de settings.py
+logging.basicConfig(filename=settings.PATH_LOG,
+                    format='%(asctime)s %(message)s', level=logging.DEBUG)
+
+
 # Create your views here.
 @api_view(['GET'])
 @authentication_classes([TokenAuthentication])
 @permission_classes([IsAuthenticated])
 def listar_datos(request):
+    logging.info('login: Se hace petición por el método: ' + request.method)
         if request.method == "GET":
             cpu = dar_uso_cpu()
             memoria = dar_uso_memoria()
             disco = dar_uso_disco()
             datos_raw = Monitor(cpu,memoria,disco)
+            logging.info('login: datos: ' + datos_raw)
             serialisador = monitorSerializer(datos_raw)
             datosServer = JSONRenderer().render(serialisador.data)
             return Response(datosServer)
